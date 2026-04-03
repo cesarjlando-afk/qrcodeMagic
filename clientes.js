573a88d9aea9a12baeb8238a3240954f02ac1f2f
cesarjlando-afk <seu@email.com>
2026-04-03 13:06:22 -0300

    new ajuste

--- clientes.js
+++ clientes.js
@@ -1,18 +1,14 @@
 const clientes = {
-
-joao:{
-mensagem:"🎉 Parabéns João!",
-video:"videos/joao.mp4"
-},
-
-maria:{
-mensagem:"💖 Convite especial para Maria",
-video:"videos/maria.mp4"
-},
-
-cliente3:{
-mensagem:"🎈 Você recebeu um convite especial!",
-video:"videos/cliente3.mp4"
-}
-
+    joao:{
+        mensagem:"🎉 Parabéns João!",
+        video:"videos/joao.mp4"
+    },
+    maria:{
+        mensagem:"💖 Convite especial para Maria",
+        video:"videos/maria.mp4"
+    },
+    cliente3:{
+        mensagem:"🎈 Você recebeu um convite especial!",
+        video:"videos/cliente3.mp4"
+    }
 }
